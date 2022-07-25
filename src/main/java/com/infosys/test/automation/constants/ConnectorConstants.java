807973ececc60a087a6b1a8e419a9437c8161c56
PrakashGopalsamy <prakash91srec@gmail.com>
2022-07-25 17:26:42 +0530

    connector constants added

---
+++ src/main/java/com/infosys/test/automation/constants/ConnectorConstants.java
@@ -0,0 +1,7 @@
+package com.infosys.test.automation.constants;
+
+public interface ConnectorConstants {
+    public static final String SOURCECOLUMNS = "sourcecolumns";
+    public static final String FILENAME = "filename";
+    public static final String COLUMNDELIMITER = "columndelimiter";
+}
